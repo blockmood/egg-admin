395e00b2d31564b24d5bbab2fe18d8335c0499f9
blockmood <yuexingphp@gmail.com>
2020-05-27 13:41:06 +0800

    update

--- app/controller/cate.js
+++ app/controller/cate.js
@@ -3,25 +3,11 @@
 const Controller = require("egg").Controller;
 const { CODE, ERROR_INFO } = require("../constans/code");
 
-const selectRule = {
-  page: { type: "string", required: true },
-  pageSize: { type: "string", required: true },
-};
-
-const createRule = {
-  cate_name: { type: "string", required: true },
-};
-
-const deleteRule = {
-  id: { type: "string", required: true },
-};
-
 class CateController extends Controller {
   async list() {
     const { ctx } = this;
-    ctx.validate(selectRule, ctx.request.body);
     let { page, pageSize } = ctx.request.body;
-    let result = await this.service.cate.select(page, pageSize);
+    let result = await this.service.cate.select(page - 1, pageSize);
     ctx.body = {
       status: CODE.SUCCESS,
       total: result.total,
@@ -30,7 +16,6 @@ class CateController extends Controller {
   }
   async create() {
     const { ctx } = this;
-    ctx.validate(createRule, ctx.request.body);
     const { cate_name } = ctx.request.body;
     let result = await this.service.cate.create(cate_name);
     ctx.body = {
@@ -40,7 +25,6 @@ class CateController extends Controller {
   }
   async update() {
     const { ctx } = this;
-    ctx.validate(deleteRule, ctx.request.body);
     const { id, cate_name } = ctx.request.body;
     let result = await this.service.cate.update(id, cate_name);
     ctx.body = {
@@ -50,7 +34,6 @@ class CateController extends Controller {
   }
   async delete() {
     const { ctx } = this;
-    ctx.validate(deleteRule, ctx.request.body);
     const { id } = ctx.request.body;
     let result = await this.service.cate.delete(id);
     ctx.body = {
