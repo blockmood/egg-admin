0bba65ff3e12935c3884e39d4ff1c1de69e2a9da
blockmood <yuexingphp@gmail.com>
2020-05-28 16:05:27 +0800

    update

--- error.sql
+++ error.sql
@@ -1,3 +1,8 @@
 USE mysql;
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 FLUSH PRIVILEGES;
+
+
+INSERT INTO `xz_user` VALUES (1, 'yuexing', '3f0506ab14f6a2a6ca12d8623bb97c32');
+
+INSERT INTO `xz_admin` VALUES (1, 'yuexing', '');
